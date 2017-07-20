b411e4692c8aa8984c4b05f2f98b510b818a9cf5
Sean Handley <sean.handley@gmail.com>
2017-07-20 20:27:32 +0100

    DRY up our solution.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,45 +6,45 @@ class GildedRose
 
   def update_backstage_pass(item)
     if item.sell_in > 10
-      item.quality += 1 if item.quality < 50
+      item.quality += 1
     elsif item.sell_in > 5
-      item.quality += 2 if item.quality < 50
+      item.quality += 2
     elsif item.sell_in > 0
-      item.quality += 3 if item.quality < 50
+      item.quality += 3
     end
     item.sell_in -= 1
     item
   end
 
   def update_brie(item)
-    item.quality += 1 if item.quality < 50
+    item.quality += 1
     item.sell_in -= 1
     item    
   end
 
-  def update_standard_item(item)
+  def update_standard_item_or_conjured(item, factor)
     item.sell_in -= 1
-    item.quality -= 1 if item.sell_in >= 0
-    item.quality -= 2 if item.sell_in < 0
+    item.quality -= factor   if item.sell_in >= 0
+    item.quality -= 2*factor if item.sell_in < 0
     item.quality = 0 if item.quality < 0
     item
   end
 
+  def update_standard_item(item)
+    update_standard_item_or_conjured(item, 1)
+  end
+
   def update_conjured(item)
-    item.sell_in -= 1
-    item.quality -= 2 if item.sell_in >= 0
-    item.quality -= 4 if item.sell_in < 0
-    item.quality = 0 if item.quality < 0
-    item   
+    update_standard_item_or_conjured(item, 2) 
   end
 
   def update_quality()
     @items.each_with_index do |item, i|
       case item.name
       when "Backstage passes to a TAFKAL80ETC concert"
-        @items[i] = update_backstage_pass(item)
+        @items[i] = update_backstage_pass(item) unless item.quality >= 50
       when "Aged Brie"
-        @items[i] = update_brie(item)
+        @items[i] = update_brie(item) unless item.quality >= 50
       when "Sulfuras, Hand of Ragnaros"
         # do nothing
       when "Conjured Mana Cake"
