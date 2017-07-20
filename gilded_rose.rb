4d9011008eaec6bc7ccd246319dea9098be88826
Sean Handley <sean.handley@gmail.com>
2017-07-20 20:54:02 +0100

    Use decorators!

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,55 +4,25 @@ class GildedRose
     @items = items
   end
 
-  def update_backstage_pass(item)
-    if item.sell_in > 10
-      item.quality += 1
-    elsif item.sell_in > 5
-      item.quality += 2
-    elsif item.sell_in > 0
-      item.quality += 3
-    end
-    item.sell_in -= 1
-    item
-  end
-
-  def update_brie(item)
-    item.quality += 1
-    item.sell_in -= 1
-    item    
-  end
-
-  def update_standard_item_or_conjured(item, factor)
-    item.sell_in -= 1
-    item.quality -= factor   if item.sell_in >= 0
-    item.quality -= 2*factor if item.sell_in < 0
-    item.quality = 0 if item.quality < 0
-    item
-  end
-
-  def update_standard_item(item)
-    update_standard_item_or_conjured(item, 1)
-  end
-
-  def update_conjured(item)
-    update_standard_item_or_conjured(item, 2) 
-  end
-
-  def update_quality()
-    @items.each_with_index do |item, i|
+  def strategies
+    @items.map do |item|
       case item.name
       when "Backstage passes to a TAFKAL80ETC concert"
-        @items[i] = update_backstage_pass(item) unless item.quality >= 50
+        BackStageStrategy.new(item)
       when "Aged Brie"
-        @items[i] = update_brie(item) unless item.quality >= 50
+        BrieStrategy.new(item)
       when "Sulfuras, Hand of Ragnaros"
-        # do nothing
+        SulfurasStrategy.new(item)
       when "Conjured Mana Cake"
-        @items[i] = update_conjured(item)
+        ConjuredItemStrategy.new(item)
       else
-        @items[i] = update_standard_item(item)
+        ItemStrategy.new(item)
       end
     end
+  end
+
+  def update_quality
+    strategies.each(&:update_quality)
   end
 end
 
@@ -60,12 +30,64 @@ class Item
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
-    @name = name
+    @name    = name
     @sell_in = sell_in
     @quality = quality
   end
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class ItemStrategy
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    @item.sell_in -= 1
+    @item.quality -= 1   if @item.sell_in >= 0
+    @item.quality -= 2   if @item.sell_in < 0
+    @item.quality = 0    if @item.quality < 0
+    @item
+  end
+end
+
+class ConjuredItemStrategy < ItemStrategy
+  def update_quality
+    @item.sell_in -= 1
+    @item.quality -= 2   if @item.sell_in >= 0
+    @item.quality -= 4   if @item.sell_in < 0
+    @item.quality = 0    if @item.quality < 0
+    @item
+  end
+end
+
+class BrieStrategy < ItemStrategy
+  def update_quality
+    return @item if @item.quality >= 50
+    @item.quality += 1
+    @item.sell_in -= 1
+    @item
+  end
+end
+
+class SulfurasStrategy < ItemStrategy
+  def update_quality ; end
+end
+
+class BackStageStrategy < ItemStrategy
+  def update_quality
+    return @item if @item.quality >= 50
+    if @item.sell_in > 10
+      @item.quality += 1
+    elsif @item.sell_in > 5
+      @item.quality += 2
+    elsif @item.sell_in > 0
+      @item.quality += 3
+    end
+    @item.sell_in -= 1
+    @item
   end
 end
